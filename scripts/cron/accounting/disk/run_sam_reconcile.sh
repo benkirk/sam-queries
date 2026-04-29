e75ac68ebfbcbcdfdd3076a2ceb619804afc6441
Ben Kirk <benkirk@ucar.edu>
2026-04-29 16:11:30 -0600

    exit success for now

--- scripts/cron/accounting/disk/run_sam_reconcile.sh
+++ scripts/cron/accounting/disk/run_sam_reconcile.sh
@@ -3,6 +3,4 @@
 # Runs sam-admin accounting --disk with env setup, passing all args through.
 source "$(dirname "${BASH_SOURCE[0]}")/../common.sh"
 
-time sam-admin accounting --disk "$@" --verbose --skip-errors && exit 0
-echo "Completed with errors"
-exit 0
+time sam-admin accounting --resource Campaign_Store --verify-paths --reconcile-quotas "$@"
